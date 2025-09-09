7dedb7ddcf80165e0ff368d7f8325f401c09f9d9
Shahzaib Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2025-09-08 20:05:16 -0500

    .

--- src/app/components/FooterMobile.tsx
+++ src/app/components/FooterMobile.tsx
@@ -1,45 +1,47 @@
-import React from 'react';
-import LogoWhite from 'asset/img/logo-white-2.png';
-import Image from 'next/image';
-import { FOOTER_ITEMS } from '../constant';
-import Telegram from 'asset/img/telegram-white.png';
-import Media from 'asset/img/media-white.png';
-import Twitter from 'asset/img/twitter-white.png';
-import Link from 'next/link';
+import React from "react";
+import LogoWhite from "asset/img/logo-white-2.png";
+import Image from "next/image";
+import { FOOTER_ITEMS } from "../constant";
+import Telegram from "asset/img/telegram-white.png";
+import Media from "asset/img/media-white.png";
+import Twitter from "asset/img/twitter-white.png";
+import Link from "next/link";
 const FooterMobile = () => {
-    return (
-        <footer className='py-12 px-5 block md:hidden bg-linear rounded-t-secondary'>
-                <div className='flex justify-center items-center mb-12'>
-                <Image src={LogoWhite} alt='Bnz logo' width={176} height={64} />
-                </div>
-                <ul className='flex gap-[33px] items-center justify-center'>
-                            <li>
-                                <Link href='https://www.reddit.com/r/BNZcrypto/s/2elKDU9llX'>
-                                    <Image src={Media} alt='Media' />
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href='https://x.com/i/communities/1829246913323119013'>
-                                    <Image src={Twitter} alt='Twitter' />
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href='https://t.me/+z6RqDniDbLAxM2U0'>
-                                    <Image src={Telegram} alt='Telegram' />
-                                </Link>
-                            </li>
-                        </ul>
-                        <hr className='my-12'/>
-                        <div className='flex items-center justify-center mb-5 gap-5 text-white/[72%]'>
-                        <Link href='/privacy'>Privacy Policy</Link>
-                        <Link href='/disclaimer'>Disclaimer</Link>
-                        </div>
-                        <div className='text-center text-white/[72%]'>
-                        <Link href='/term'>Terms and Conditions</Link>
-                        </div>
-                <p className='text-base font-normal text-white/70 text-center mt-8'>All rights reserved</p>
+  return (
+    <footer className="py-12 px-5 block md:hidden bg-linear rounded-t-secondary">
+      <div className="flex justify-center items-center mb-12">
+        <Image src={LogoWhite} alt="Bnz logo" width={176} height={64} />
+      </div>
+      {/* <ul className="flex gap-[33px] items-center justify-center">
+        <li>
+          <Link href="https://www.reddit.com/r/BNZcrypto/s/2elKDU9llX">
+            <Image src={Media} alt="Media" />
+          </Link>
+        </li>
+        <li>
+          <Link href="https://x.com/i/communities/1829246913323119013">
+            <Image src={Twitter} alt="Twitter" />
+          </Link>
+        </li>
+        <li>
+          <Link href="https://t.me/+z6RqDniDbLAxM2U0">
+            <Image src={Telegram} alt="Telegram" />
+          </Link>
+        </li>
+      </ul> */}
+      <hr className="my-12" />
+      <div className="flex items-center justify-center mb-5 gap-5 text-white/[72%]">
+        <Link href="/privacy">Privacy Policy</Link>
+        <Link href="/disclaimer">Disclaimer</Link>
+      </div>
+      <div className="text-center text-white/[72%]">
+        <Link href="/term">Terms and Conditions</Link>
+      </div>
+      <p className="text-base font-normal text-white/70 text-center mt-8">
+        All rights reserved
+      </p>
 
-            {/* <div className='flex justify-between xl:flex-nowrap flex-wrap md:gap-3 md:flex-row flex-col lg:gap-6'>
+      {/* <div className='flex justify-between xl:flex-nowrap flex-wrap md:gap-3 md:flex-row flex-col lg:gap-6'>
                 <div>
                     <ul className='hidden md:flex gap-5 text-white text-base font-normal flex-wrap xl:flex-nowrap'>
                         {FOOTER_ITEMS.map((item) => (
@@ -84,8 +86,8 @@ const FooterMobile = () => {
                     </li>
                 </ul>
             </div> */}
-        </footer>
-    );
+    </footer>
+  );
 };
 
 export default FooterMobile;
